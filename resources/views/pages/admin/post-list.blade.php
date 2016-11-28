b0fde394d7c40d7c240f7f1db6b5b826a76d9a47
fletchynet <bitbucket@fletchy.net>
2016-11-29 00:18:02 +1100

    Add link to main screen

--- resources/views/pages/admin/post-list.blade.php
+++ resources/views/pages/admin/post-list.blade.php
@@ -72,6 +72,8 @@
                             <td>
                                 <h4>{{$post->title}}</h4>
                                 {{$post->description}}
+                                <br><br>
+                                <a href="{{ url($post->slug) }}">{{ url($post->slug) }}</a>
                             </td>
                             <td>
                                 <span class="label border-left-violet label-striped">{{$post->category_name}}</span>
