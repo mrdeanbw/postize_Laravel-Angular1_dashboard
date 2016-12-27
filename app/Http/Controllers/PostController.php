303d42c6830c9e50ea857ae98ec2009e185f247e
fletchynet <bitbucket@fletchy.net>
2016-12-27 14:06:09 +1100

    Now pleases search engines: Articles by default are now all on 1 page, but with the precense of utm_source parameter, splits into multiple pages for the remainder of the session

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,6 +9,7 @@ use App\Models\UrlHelpers;
 use Agent;
 use Auth;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 use View;
 use DB;
 
@@ -41,75 +42,39 @@ class PostController extends Controller
         $numberOfPagesInArticle = 1;
         $pages = [];
         $currentPageContent = [];
-        if($request->get('test')) {
-            $pageCount = 1;
-            for($i = 0; $i < count($blockContent); $i++) {
-                if ($blockContent[$i]->type == 'pagebreak') {
-                    $pageCount++;
-                }
-            }
-
-            for($i = 0; $i < count($blockContent); $i++) {
-                if ($blockContent[$i]->type == 'pagebreak') {
-                    $pages[] = $currentPageContent;
-                    $currentPageContent = [];
-                } else {
-                    if ($blockContent[$i]->type == 'image') {
-                        $blockContent[$i]->content = '';
-
-                        if(!empty($blockContent[$i]->title))
-                            $blockContent[$i]->content .= '<h2>' . $blockContent[$i]->title . '</h2>';
-
-                        if(!empty($blockContent[$i]->description))
-                            $blockContent[$i]->content .= '<p>' . $blockContent[$i]->description . '</p>';
-
-                        $blockContent[$i]->content .= '<img src="' . $blockContent[$i]->url . '" />';
-
-                        if(!empty($blockContent[$i]->source) && !empty($blockContent[$i]->sourceurl)) {
-                            $blockContent[$i]->content .= '<span class="source"><span>via:</span><a href="' .
-                                $blockContent[$i]->sourceurl . '" target="blank">' . $blockContent[$i]->source . '</a></span>';
-                        }
-                    }
-
-                    $currentPageContent[] = $blockContent[$i];
-                }
-
-                if($i == count($blockContent) - 1) {
-                    $pages[] = $currentPageContent;
-                    $currentPageContent = [];
-                }
-            }
+        $utmSourceParameter = $request->get('utm_source', Session::get('source'));
+        if($utmSourceParameter) {
+            Session::put('source', $utmSourceParameter);
         }
-        else {
-            for($i = 0; $i < count($blockContent); $i++) {
-                if ($blockContent[$i]->type == 'pagebreak') {
-                    $pages[] = $currentPageContent;
-                    $currentPageContent = [];
-                } else {
-                    if ($blockContent[$i]->type == 'image') {
-                        $blockContent[$i]->content = '';
 
-                        if(!empty($blockContent[$i]->title))
-                            $blockContent[$i]->content .= '<h2>' . $blockContent[$i]->title . '</h2>';
+        for ($i = 0; $i < count($blockContent); $i++) {
+            if ($utmSourceParameter && $blockContent[$i]->type == 'pagebreak') {
+                $pages[] = $currentPageContent;
+                $currentPageContent = [];
+            } else {
+                if ($blockContent[$i]->type == 'image') {
+                    $blockContent[$i]->content = '';
 
-                        if(!empty($blockContent[$i]->description))
-                            $blockContent[$i]->content .= '<p>' . $blockContent[$i]->description . '</p>';
+                    if (!empty($blockContent[$i]->title))
+                        $blockContent[$i]->content .= '<h2>' . $blockContent[$i]->title . '</h2>';
 
-                        $blockContent[$i]->content .= '<img src="' . $blockContent[$i]->url . '" />';
+                    if (!empty($blockContent[$i]->description))
+                        $blockContent[$i]->content .= '<p>' . $blockContent[$i]->description . '</p>';
 
-                        if(!empty($blockContent[$i]->source) && !empty($blockContent[$i]->sourceurl)) {
-                            $blockContent[$i]->content .= '<span class="source"><span>via:</span><a href="' .
-                                $blockContent[$i]->sourceurl . '" target="blank">' . $blockContent[$i]->source . '</a></span>';
-                        }
+                    $blockContent[$i]->content .= '<img src="' . $blockContent[$i]->url . '" />';
+
+                    if (!empty($blockContent[$i]->source) && !empty($blockContent[$i]->sourceurl)) {
+                        $blockContent[$i]->content .= '<span class="source"><span>via:</span><a href="' .
+                            $blockContent[$i]->sourceurl . '" target="blank">' . $blockContent[$i]->source . '</a></span>';
                     }
-
-                    $currentPageContent[] = $blockContent[$i];
                 }
 
-                if($i == count($blockContent) - 1) {
-                    $pages[] = $currentPageContent;
-                    $currentPageContent = [];
-                }
+                $currentPageContent[] = $blockContent[$i];
+            }
+
+            if ($i == count($blockContent) - 1) {
+                $pages[] = $currentPageContent;
+                $currentPageContent = [];
             }
         }
 
@@ -117,14 +82,14 @@ class PostController extends Controller
         if ($pageNumber > count($pages))
             return redirect()->to('/');
 
-        $post->blocks = $pages[$pageNumber - 1];
+        $post->blocks = !$utmSourceParameter ? $pages[0] : $pages[$pageNumber - 1]; // Uses $pages[0] because without a utm_source parameter, the entire article is shown as 1 page.
         $post->is_last_page = count($pages) == $pageNumber;
 
         $nextPageUrl = url($slug) . '/' . ($pageNumber + 1);
-        $urlParts = parse_url($request->fullUrl());
-        if(!empty($urlParts['query'])) {
+        /*$urlParts = parse_url($request->fullUrl());
+        if (!empty($urlParts['query'])) {
             $nextPageUrl .= '?' . $urlParts['query'];
-        }
+        }*/
 
         return view('pages.post')
             ->with('post', $post)
